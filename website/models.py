b117eb89cd9ec3b3eb2822a7ebabeb21768965c4
ds7cw <delyan.shivachev@hotmail.com>
2023-09-27 21:28:49 +0100

    Model Added

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    games_played = models.IntegerField(default=0)
+    ppg = models.FloatField(default=0.0)
+    rpg = models.FloatField(default=0.0)
+    apg = models.FloatField(default=0.0)
+    fg_pct = models.FloatField(default=0.0)
+    fg3_pct = models.FloatField(default=0.0)
+    ft_pct = models.FloatField(default=0.0)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name} [{self.ppg} Points Per Game]"
