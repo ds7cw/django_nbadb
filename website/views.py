eb6f7b445b9f62cf738442622a44e983cb7da713
ds7cw <delyan.shivachev@hotmail.com>
2023-10-01 18:01:32 +0100

    First template added

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Player
 
 # Create your views here.
+
+def players(request):
+    records = Player.objects.all()
+
+    return render(request, 'players.html', {'records': records})
