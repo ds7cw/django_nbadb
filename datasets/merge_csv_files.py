5f48e50b9a2dc7e7776156e8e81785a8efafcd01
ds7cw <delyan.shivachev@hotmail.com>
2023-10-04 21:51:10 +0100

    Added main dataset containing players' stats and contract info

---
+++ datasets/merge_csv_files.py
@@ -0,0 +1,24 @@
+with open('stats.csv', 'r') as stats_file:
+    stats_data = stats_file.readlines()
+
+with open('contracts.csv', 'r') as contracts_file:
+    contracts_data = contracts_file.readlines()
+
+contracts_dict = {}
+
+for line in contracts_data:
+    k, v = line.strip().split(',')
+    contracts_dict[k] = v
+
+stats_list = []
+
+for row in stats_data[1:]:
+
+    player_name = ' '.join(row.split(',')[:2])
+
+    salary = contracts_dict.get(player_name, '0')
+
+    row_and_salary = row.strip() + f',{salary}\n'
+
+    with  open('dataset.txt', 'a') as file:
+        file.write(row_and_salary)
