27adfc3af89c63ca5d585e915cc740a215199030
ds7cw <delyan.shivachev@hotmail.com>
2023-10-26 15:57:54 +0100

    Added MainPlayerAdmin class, minor cosmetic changes for admin site

--- website/admin.py
+++ website/admin.py
@@ -4,4 +4,12 @@ from django.contrib import admin
 from .models import Player, MainPlayer
 
 admin.site.register(Player)
-admin.site.register(MainPlayer)
+# admin.site.register(MainPlayer)
+
+@admin.register(MainPlayer)
+class MainPlayerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'suffix',
+                    'pos', 'team', 'rpg', 'apg', 'ppg'
+                    ]
+    search_fields = ['first_name', 'last_name', 'suffix']
+    list_filter = ['team', 'pos']
